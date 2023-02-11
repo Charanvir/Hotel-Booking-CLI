e91508a88bce290377086e28c292f6324762dbdf
Charanvir <charanvir123@gmail.com>
2023-02-11 03:58:55 -0500

    implemented additional SPA class

--- main.py
+++ main.py
@@ -62,9 +62,29 @@ class SecureCreditCard(CreditCard):
             return False
 
 
+class SpaHotel(Hotel):
+    def book_spa_package(self):
+        pass
+
+
+class SpaTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you you for your SPA reservation!
+        Here is your SPA booking data:
+        Name: {self.customer}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_ID)
+hotel = SpaHotel(hotel_ID)
 
 if hotel.available():
     # these would be inputted regularly by the user
@@ -75,6 +95,11 @@ if hotel.available():
             name = input("Enter your name: ")
             reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
             print(reservation_ticket.generate())
+            spa = input("Do you want to book a spa package: ").lower()
+            if spa == "yes":
+                hotel.book_spa_package()
+                spa_ticket = SpaTicket(customer_name=name, hotel_object=hotel)
+                print(spa_ticket.generate())
         else:
             print(f"""
                     There was a problem with your payment
